a589301a7df22c526d7a8644178fbfe93671289e
ms-solly <119148636+ms-solly@users.noreply.github.com>
2023-10-04 23:20:48 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const taskInput = document.getElementById("task");
     const addBtn = document.getElementById("add");
     const taskList = document.getElementById("task-list");
+    
 
     addBtn.addEventListener("click", function() {
         const taskText = taskInput.value.trim();
